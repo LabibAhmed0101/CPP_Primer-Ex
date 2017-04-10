f22e28473db9cf33b38bee1199079d62b2eda33c
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2017-04-10 14:41:21 +0200

    Update Exercise 3.23.cpp

--- Chapter 3/Exercise 3.23.cpp
+++ Chapter 3/Exercise 3.23.cpp
@@ -7,10 +7,10 @@ using std::endl;
 
 int main()
 {
-	vector<int> numbers{ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };	//declaring and intializing a vector of int.
-	for (vector<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter) //going through the vector using iterators
+	vector<int> numbers{ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };	//Declaring and intializing a vector of int.
+	for (vector<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter) //Going through the vector using iterators
 	{
-		*iter = 2 * (*iter);		//assiging the new value pointed to by iter after doubling it using derefence operator.
+		*iter = 2 * (*iter);		//Assiging the new value pointed to by iter after doubling it using derefence operator.
 		cout << *iter << " ";
 	}
 
