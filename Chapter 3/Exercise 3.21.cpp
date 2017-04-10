b5f0d14a8db075127a5ba6ed47b76149b4c868a1
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2017-04-10 13:47:33 +0200

    Create Exercise 3.21.cpp

---
+++ Chapter 3/Exercise 3.21.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+	vector<int> numbers{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };		//Declaring and intializing a vector
+	
+	for (vector<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter) //Going through the vector using iterator
+	{
+		*iter *= *iter;		//Squaring the value iter pointing to
+	}
+	
+	for (vector<int>::iterator iter = numbers.begin(); iter != numbers.end(); ++iter)
+	{
+		cout << *iter << " ";	//sending the value pointed to by iter to the std output
+	}
+
+	cout << endl;
+	return 0;
+}
