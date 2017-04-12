467aecc2c70d612344bd24b568ee5166b5b87114
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2017-04-12 15:14:23 +0200

    Create Exercise 3.28.cpp

---
+++ Chapter 3/Exercise 3.28.cpp
@@ -0,0 +1,13 @@
+//Exercise 3.28:
+//What are the values in the following arrays?
+
+string sa[10];		// 10 empty strings(default intialized).
+
+int ia[10];		// 10 elements each is 0  if its defined outside a function otherwise if defined inside main or another
+			//function will have 10 undefined -garbage- values
+			
+int main() {
+string sa2[10];		// 10 empty strings default initialized
+	
+int ia2[10];		// 10 integer elements have undefined - garabage - values . because its defined in main().	
+}
