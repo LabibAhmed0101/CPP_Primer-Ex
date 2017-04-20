0fc5cf6ef7f9192f015ded6b9961a99e49f5edb5
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2017-04-20 10:06:49 +0200

    Update Exercise 3.39.cpp

--- Chapter 3/Exercise 3.39.cpp
+++ Chapter 3/Exercise 3.39.cpp
@@ -36,3 +36,64 @@ int main()
 }
 
 //b)Program to compare two C-style character strings
+
+#include<iostream>
+#include<cstddef>		//Included to enable using size_t
+#include<cstring>		//Included to enable using cstring function strcmp()
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	constexpr size_t MAX_SIZE = 100;			//Max size of the cstring
+
+	char arr1[MAX_SIZE], arr2[MAX_SIZE];			//Declaring two arrays of characters (cstring)
+
+	cout << "Enter the 1st string" << endl;			
+	cin.getline(arr1, MAX_SIZE);
+
+	cout << "Enter the 2nd string" << endl;
+	cin.getline(arr2, MAX_SIZE);
+
+	if (strcmp(arr1, arr2) == 0)				//strcmp returns 0 if both cstring are equal
+		cout << "\nThe 1st and the 2nd string are equal" << endl;
+	else if (strcmp(arr1, arr2) < 0)			//strcmp returns negative value if arr1 is smaller than arr2
+		cout << "\nThe 1st string is smaller than the 2nd string" << endl;
+	else							//strcmp returns positve value if arr1 is bigger than arr2
+		cout << "\nThe 1st string is bigger than the 2nd string" << endl;
+
+	return 0;
+}
+
+/*cin.getline let the user input a full line in cstring as getline() do with string
+it is also much better to use than cin >> becuase with cin >> there will be over flow problem if the user input more than 99 char .
+
+cin.getline also makes it possiable to use a delimiter instead of the default delimiter which is '\n'.
+Delimiter is used to define where the cstring input stream ends by default if the user pressed enter while he inputs the string, that 
+will end the stream of inputs for example :
+
+if the user is  inputing address inside a cstring array it have to  be like that:
+20
+London st.
+London
+United Kingdom
+
+Here the new line is a part of the string so we must use a delimiter so the stream of char won't take enter/return as the end of the 
+stream. 
+We can set a new delimiter instead of the default one through cin.getline(arr,MAXSIZE,'$')
+the 1st argument is the cstring variable "the array of characters",
+the 2nd argument is the cstring max size ,
+the 3rd argument is the new delimiter so enter/return is not the current delimiter and it is now treated as any other character and the 
+stream only will end when the user hits '$' user stream of iputs would be like that.
+
+20
+London st.
+London
+United Kingdom$
+
+stream will end when the user type $.
+
+*/
+
