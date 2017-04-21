b6c705e3a1be95ac8674ad88547fa255e8d7d7b4
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2017-04-21 12:08:00 +0200

    Update Exercise 3.42.cpp

--- Chapter 3/Exercise 3.42.cpp
+++ Chapter 3/Exercise 3.42.cpp
@@ -1,3 +1,41 @@
 /*Exercise 3.42: 
 Write a program to copy a vector of ints into an array of
 ints.*/
+
+#include<iostream>
+#include<vector>	
+
+using std::cout;
+using std::endl;
+using std::vector;    
+
+int main()
+{
+	vector<int> vec = { 1,2,3,4,5,6,7,8,9,10 };		//Declaring and intializing a vector 
+
+	constexpr size_t ARR_SIZE = 10;				//Defining the array size
+	int arr[ARR_SIZE];					//Declaring the array
+
+	int i = 0;						//Declaring and intializing counter for the array elements
+	for (vector<int>::const_iterator iter = vec.begin() ; iter != vec.end() && i < ARR_SIZE; ++iter, ++i)
+	{
+		arr[i] = *iter;					//Each element in the vector assigned to the correspondant element in the
+	}							//array
+
+	cout << "The current array elements are " << endl;
+	for (int i = 0; i < ARR_SIZE; ++i)			//Sending the array elements to the output
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+	return 0;
+}
+//code from line 19 to line 23 could be replaced using subscript operator with vectors :
+
+for(int i = 0 ; i < Arr_SIZE : ++i)
+{
+	arr[i] = vec[i];
+}
+
+//vector<int>::const_iterator could be replace with auto in the c++11 standard.
