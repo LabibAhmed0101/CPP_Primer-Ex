f3787eeea5d7b42c77423288ab066a637699cfd8
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2018-10-26 13:32:52 +0200

    Update Exercise 4.6.cpp

--- Chapter 4/Exercise 4.6.cpp
+++ Chapter 4/Exercise 4.6.cpp
@@ -2,3 +2,25 @@
 Exercise 4.6: 
 Write an expression to determine whether an int value is even or odd.
 */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int value = 0;
+	cout << "Enter a number to check if even or odd :\n" << endl;
+	cin >> value;
+
+	if (value % 2 == 0)
+	{
+		cout << value << " is even" << endl;
+	}
+	else
+	{
+		cout << value << " is odd" << endl;
+	}
+
+	return 0;
+}
