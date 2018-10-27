1b12a6a6a005452afa80bab876b7fd33b276c66f
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2018-10-27 10:59:07 +0200

    Create Exercise 3.6.cpp

---
+++ Chapter 3/Exercise 3.6.cpp
@@ -0,0 +1,23 @@
+/*
+Exercise 3.6: 
+Use a range for to change all the characters in a string to X.
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string str("password");
+
+	for (char &c : str)
+	{
+		c = 'X';
+	}
+
+	cout << "new str is " << str << endl;
+
+	return 0;
+}
