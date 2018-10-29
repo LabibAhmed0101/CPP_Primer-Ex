9d5443ecebff5ca0a4312977563d7f45eb02e683
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2018-10-29 09:28:24 +0200

    Create Exercise 4.10.cpp

---
+++ Chapter 4/Exercise 4.10.cpp
@@ -0,0 +1,12 @@
+/*
+Exercise 4.10: 
+Write the condition for a while loop that would read ints from the standard input and 
+stop when the value read is equal to 42.
+*/
+
+int x;
+cout << "Please enter some numbers : " << endl;
+while (cin >> x && x != 42)
+{
+// code
+}
