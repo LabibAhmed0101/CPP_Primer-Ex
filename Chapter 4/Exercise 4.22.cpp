b380f15ba46f348c63d3aab1b817d3f2431cef14
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2018-11-06 15:25:43 +0200

    Update Exercise 4.22.cpp

--- Chapter 4/Exercise 4.22.cpp
+++ Chapter 4/Exercise 4.22.cpp
@@ -6,3 +6,28 @@ two versions: One version that uses only conditional operators; the other
 should use one or more if statements. Which version do you think is easier
 to understand and why?
 */
+
+// Using conditional operators
+
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+	int grade = 0;
+	cout << "Please enter ur current grade :";
+	cin >> grade;
+
+	cout << "\nYou got a : ";
+	
+	//Conditional operator to determine high pass / pass / low pass / fail
+	
+	cout << (grade > 90 ? "High Pass" : grade > 75 ? "Pass" : grade > 60 ? "Low Pass" : "fail") << endl;		
+
+	return 0;
+}
+
