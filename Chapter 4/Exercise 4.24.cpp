a170a69d49001ff1d03672dbbf39b30ebba38856
LabibAhmed0101 <labib.ahmed0101@gmail.com>
2018-11-08 17:05:35 +0200

    Update Exercise 4.24.cpp

--- Chapter 4/Exercise 4.24.cpp
+++ Chapter 4/Exercise 4.24.cpp
@@ -1,3 +1,4 @@
+/*
 Exercise 4.24: 
 Our program that distinguished between high pass, pass,
 and fail depended on the fact that the conditional operator is right
@@ -18,11 +19,11 @@ means that its 3 operands will be groupped from right to left like that
 
 finalgrade = (grade > 90) ? "high pass" : ((grade < 60) ? "fail" : "pass");
 
-the 3 operands of the main conditional operator
+the 3 operands of the main conditional operator (the 1st ?: ) are 
 
 1.(grade > 90)				//expr1 is the 1st operand
 2."high pass"				//expr2 is the 2nd operand , will be evaluated ONLY WHEN expr1 is true
-3.((grade < 60) ? "fail" : "pass")	//expr3  is the 3rd operand , will be evaluated ONLY WHEN expr1 is false
+3.((grade < 60) ? "fail" : "pass")	//expr3 is the 3rd operand , will be evaluated ONLY WHEN expr1 is false
 
 notice that 3rd operand will be ONLY EVALUATED if the 1st operand is false
 
@@ -40,11 +41,37 @@ will be like
 
 finalgrade = ((grade > 90) ? "high pass" : (grade < 60)) ? "fail" : "pass";
 
+the 3 operands of the 1st conditional operator (?:) are
 
+1.(grade > 90)			//operand 1
+2."high pass"			//operand 2
+3.(grade < 60)			//operand 3
 
+operand 3 here INSTEAD OF BEING the  2nd conditionl operator expression like when it was right associtive 
+((grade < 60) ? "fail" : "pass") so it was ignored when finalgrade > 90
 
+it is (grade < 60) as a result of being left associative
 
+This way the program will have a problem when finalgrade > 90 and finalgrade will be  "fail"
+instead of being "high pass" and thats why it will be like that.
 
+when finalgrade > 90 then expr1 "high pass" will be evaluated and it will be like 
+if we type that.
+
+("high pass") ? "fail" : "pass"
+
+since "high pass" is not empty string (none-zero value) it will be converted to 
+
+(true) ? "fail" : "pass"
+
+since expr1 in the last condition is true ,then expr2 will be evaluated which is "fail".
+
+if conditionl operator is left associative it won't work as it should do when finalgrade > 90 but
+will work just fine for the other cases like if  finalgrade < 60 or finalgrade > 60
+
+the next program shows the difference in output between left/right grouping with different grades
+
+*/
 
 #include<iostream>
 #include<string>
@@ -58,25 +85,20 @@ int main()
 	string finalGradeLeftAssociative;
 	
 
-	for (size_t i = 0; i < 3; i++)
-	{
-		cout << "Enter your grade : ";
-		cin >> grade;
+	cout << "Enter your grade : ";
+	cin >> grade;
 
-		//operands grouped from right to left
+	//operands grouped from right to left
 
-		finalGradeRightAssociative = (grade > 90) ? "high pass"
-			: ((grade < 60) ? "fail" : "pass");
+	finalGradeRightAssociative = (grade > 90) ? "high pass" : ((grade < 60) ? "fail" : "pass");
 
-		//operands grouped from left to right
+	//operands grouped from left to right
 
-		finalGradeLeftAssociative = ((grade > 90) ? "high pass"
-			: (grade < 60)) ? "fail" : "pass";
+	finalGradeLeftAssociative = ((grade > 90) ? "high pass" : (grade < 60)) ? "fail" : "pass";
 
+	
 
-
-		cout << "\nFinal grade when Right associated (normally) : " << finalGradeRightAssociative << endl;
-		cout << "\nFinal grade when Left associated	     : " << finalGradeLeftAssociative << endl;
-	}
+	cout << "\nFinal grade when Right associated (normally) : " << finalGradeRightAssociative << endl;
+	cout << "\nFinal grade when Left associated	     : " << finalGradeLeftAssociative << endl;
 	return 0;
 }
